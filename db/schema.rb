9346fdf5303f2cf14306d4ba584a3cb1909e52ad
mintthehole <johnpollo88@gmail.com>
2012-12-26 15:11:59 +0530

    sms

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20121222081139) do
+ActiveRecord::Schema.define(:version => 20121226063403) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",                                  :null => false
