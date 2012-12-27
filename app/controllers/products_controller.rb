e6319b41e0808821e97a7459b5552b3579143343
mintthehole <johnpollo88@gmail.com>
2012-12-27 17:07:41 +0530

    produts

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,10 +8,11 @@ class ProductsController < ApplicationController
   	lead = CustomerLead.build(params)
   	lead.save
     parent_product = Product.find_by_id(params[:parent_product_id])
-    SmsApi.send(params[:info],"User Prototype No:#{params[:info]} Product:#{parent_product.name.truncate(32)} Time:#{Time.zone.now}")
+    product = Product.find_by_id(params[:product_id])
+    SmsApi.send(params[:info],"User Prototype No:#{params[:info]} Product:#{product.name.truncate(32)} Time:#{Time.zone.now}")
     
     SmsApi.send(parent_product.phone_no,"#{parent_product.name} Prototype No:#{params[:info]} Time:#{Time.zone.now}")
-    product = Product.find_by_id(params[:product_id])
+    
     SmsApi.send(product.phone_no,"#{product.name} Prototype No:#{params[:info]} Time:#{Time.zone.now}")
 
   	respond_to do |format|
