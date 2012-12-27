62eb920728a15c8be30bc31dd2d757895b6f5f03
mintthehole <johnpollo88@gmail.com>
2012-12-27 18:33:45 +0530

    sms changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,9 +9,9 @@ class ProductsController < ApplicationController
   	lead.save
     parent_product = Product.find_by_id(params[:parent_product_id])
     product = Product.find_by_id(params[:product_id])
-    SmsApi.send(params[:info],"User Prototype No:#{params[:info]} Product:#{product.name.truncate(32)} Time:#{Time.zone.now}")
-    SmsApi.send(parent_product.phone_no,"#{parent_product.name.truncate(32)} Prototype No:#{params[:info]} Time:#{Time.zone.now}")
-    SmsApi.send(product.phone_no,"#{product.name.truncate(32)} Prototype No:#{params[:info]} Time:#{Time.zone.now}")
+    SmsApi.send(params[:info],"User Proto No:#{params[:info]} PID:#{product.name.truncate(32)} Dt:#{Date.today}")
+    SmsApi.send(parent_product.phone_no,"#{parent_product.name.truncate(32)} Proto No:#{params[:info]} Dt:#{Date.today}")
+    SmsApi.send(product.phone_no,"#{product.name.truncate(32)} Proto No:#{params[:info]} Dt:#{Date.today}")
 
   	respond_to do |format|
 	    format.xml { render :xml =>lead, :status => :ok }
